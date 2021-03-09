b60efa51eb9da6948042944aadb7d391bb2eba76
Esbun <78551780+esbun@users.noreply.github.com>
2021-03-09 13:26:08 +0100

    Add files via upload

---
+++ daus script/shx-snus-master/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+game 'gta5'
+
+server_script "@mysql-async/lib/MySQL.lua"
+server_script "sv_snus.lua"
+
+client_script "cl_snus.lua"
+
+this_is_a_map 'yes'
---
+++ daus script/shx-snus-master/items.sql
@@ -0,0 +1,11 @@
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('shx_stone', 'Stone', 1, 0, 1),
+	('shx_snus', 'Snus', 1, 0, 1),
+	('shx_parmul', 'Parmul', 1, 0, 1),
+	('shx_salca', 'Salca', 1, 0, 1),
+	('shx_mercan', 'Mercan', 1, 0, 1),
+	('shx_yosun', 'Yosun', 1, 0, 1),
+	('shx_odens', 'Odens', 1, 0, 1),
+	('shx_bøttesnus', 'Bøtte snus', 1, 0, 1),
+	('1gr_roka', 'Roka', 1, 0, 1)
+;
